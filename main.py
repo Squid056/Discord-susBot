779f92be482e0315b7ba84b16df0f3486a7c4bc6
Alper <106012012+Squid056@users.noreply.github.com>
2022-05-23 06:43:07 +0000

    added gif and changed reactions

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
 import os
 import discord 
-
-#client log and watching status 
+ 
 activity = discord.Activity(name='you', type=discord.ActivityType.watching)
 client = discord.Client(activity=activity)
+embed = discord.Embed()
+embed.set_image(url="https://media.discordapp.net/attachments/890017011780964442/976759712508964864/smoke-smoking.gif")
 
 @client.event
 async def on_ready():
   print('we have logged in as {0.user}'.format(client))
 
-
-
 # on message sus say 
 @client.event
 async def on_message(message):
-  #emoji = '\N{THUMBS UP SIGN}'
   emoji = '👨‍🦯'
   if message.author == client.user:
-    await message.add_reaction(emoji)
     return
 
   if message.content.startswith('sus'):
     await message.channel.send('ඞ')
+    await message.add_reaction(emoji)
 
   if message.content.startswith('hi'):
     await message.author.send('👀')
+  
+  if message.content.startswith('jalopenos'):
+    await message.channel.send(embed=embed)
+  
 
 
 token = os.environ['BotToken']
